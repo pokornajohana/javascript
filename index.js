6365af2ac4edd67c2d83d2ca9e2d732e2886d304
pokornajohana <pokorna.johana1@gmail.com>
2022-02-22 20:04:16 +0100

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import './promenne/ukol1.js';
+//import './promenne/ukol1.js';
 
 //vypočti počet minut v celém ledu.
 //vypíš vásledek na konzoli
 
-let minutvhodine = 60;
+/*let minutvhodine = 60;
 let hodinvedne = 24;
 let dnuvlednu = 31;
 
@@ -23,3 +23,22 @@ console.log('Jmenuješ se: ' + jmeno);
 let prsi = true;
 
 //if (vek >) {}
+*/
+
+let a = Number(prompt('zadejte A:'));
+let b = Number(prompt('zadejte B:'));
+let c = Number(prompt('zadejte C:'));
+
+if (a > b) {
+  if (a > c) {
+    console.log('největší číslo je A:' + a);
+  } else {
+    console.log('Největší číslo je C:' + c);
+  }
+} else {
+  if (b > c) {
+    console.log('Největší číslo je B:' + b);
+  } else {
+    console.log('Největší číslo je C:' + c);
+  }
+}
--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,33 +1,20 @@
-//vypočti počet minut v celém ledu.
-//vypíš vásledek na konzoli
+let jmenoUzivatele = prompt('Jak se jmenuješ?');
+let vekUzivatele = Number(prompt('Kolik je Ti let?'));
 
-let minutvhodine = 60;
-let hodinvedne = 24;
-let dnuvlednu = 31;
-
-let vysledek = minutvhodine * hodinvedne * dnuvlednu;
-
-console.log('v lednu je celkem ' + vysledek);
-
-let jméno = 'Petra';
-let věk = 34;
-let váha = 65;
-let výška = 1.67;
-
-let BMI = váha / (výška * výška);
-
-console.log(
-  'Jmenuji se ' +
-    jméno +
-    ' , je mi ' +
-    věk +
-    ' let, vážím ' +
-    váha +
-    ' kg, měřím ' +
-    výška +
-    ' m a moje BMI je ' +
-    BMI +
-    '. '
-);
-
-
+if (vekUzivatele < 18) {
+  console.log(
+    'Vaše jméno je ' +
+      jmenoUzivatele +
+      ', je Vám ' +
+      vekUzivatele +
+      'můžete tedy řídit'
+  );
+} else {
+  console.log(
+    'Vaše jméno je ' +
+      jmenoUzivatele +
+      ', je Vám ' +
+      vekUzivatele +
+      'Už můžete řídit auto'
+  );
+}
